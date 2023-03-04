d64bc2f88c2b938ac22ff53228e4f6c122a7ff5e
Saeed <saeed6433@gmail.com>
2023-03-03 23:30:00 -0500

    input as a module

--- play.js
+++ play.js
@@ -1,21 +1,6 @@
 const {connect} = require("./client.js")
+const {setupInput} = require("./input")
 
 console.log("Connecting ...");
 connect();
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  // return stdin;
-  stdin.on("data", handleUserInput);
-
-};
-const handleUserInput = function (key) {
-  if (key === '\u0003'){
-    process.exit();
-  }
-};
-
 setupInput()
