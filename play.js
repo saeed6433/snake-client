71215994d38de7a221603fe5519b36d5228c64e9
Saeed <saeed6433@gmail.com>
2023-03-03 22:07:09 -0500

    stdin setup - handle ctrl+c

--- play.js
+++ play.js
@@ -1,5 +1,21 @@
 const {connect} = require("./client.js")
 
-
 console.log("Connecting ...");
 connect();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  // return stdin;
+  stdin.on("data", handleUserInput);
+
+};
+const handleUserInput = function (key) {
+  if (key === '\u0003'){
+    process.exit();
+  }
+};
+
+setupInput()
